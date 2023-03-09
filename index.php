64c4ea9bc9c6d31bc902d282de65f2be53c1429e
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-09 18:11:33 +0100

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- TITLE -->
+	<title>Bad Words</title>
+	<!-- LINK -->
+</head>
+
+<body>
+	<div id="root">
+
+		<div class="container">
+            <h2>Scrivi il tuo testo</h2>
+            <form action="./badwords.php" method="GET">
+                <label for="your-text"></label>
+                <input id="your-text" type="textarea" name="your-text">
+                <button>Invia</button>
+            </form>
+		</div>
+	</div>
+
+</body>
+
+</html>
