f6ae0f2d37de5d7a45d24eeca25c35048b872c61
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-10 09:18:30 +0100

    + little style

--- index.php
+++ index.php
@@ -8,17 +8,18 @@
 	<!-- TITLE -->
 	<title>Bad Words</title>
 	<!-- LINK -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-		<div class="container">
-            <h3>Scrivi il tuo testo</h3>
+		<div class="container px-3 w-2/4">
+            <h3 class="uppercase font-bold leading-10">Scrivi il tuo testo</h3>
             <form action="./badwords.php" method="GET">
-                <label for="your-text"></label>
-                <input id="your-text" type="textarea" name="your-text">
-                <h3>Scrivi la brutta parola</h3>
-                <input id="your-badword" type="textarea" name="your-badword">
-                <button>Invia</button>
+                <label class="uppercase font-bold leading-10" for="your-text"></label>
+                <input class="border-2 border-indigo-600 p-2" id="your-text" type="textarea" name="your-text">
+                <h3 class="uppercase font-bold leading-10">Scrivi la brutta parola</h3>
+                <input class="border-2 border-indigo-600 p-2" id="your-badword" type="textarea" name="your-badword">
+                <button class="border-2 border-indigo-600 p-2">Invia</button>
             </form>
 		</div>
 
